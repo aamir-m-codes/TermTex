18002e93afd2ea05940ce8b824fd9b7900c5af33
aamir-m-codes <aamirmaqsood.codes@gmail.com>
2026-07-18 11:36:51 +0500

    updation: try to examine cursor position to get size

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ void repositionCursor();
 void refreshEditorScreen();
 void drawEditorRows();
 int getWindowSize(int *rows, int *cols);
+int getCursorPosition();
 
 /*** Data Section ***/
 struct editorConfig
@@ -83,8 +84,8 @@ int getWindowSize(int *rows, int *cols)
   {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
       return -1;
-    editorReadKey();
-    return -1;
+
+    return getCursorPosition();
   }
   else
   {
@@ -92,6 +93,29 @@ int getWindowSize(int *rows, int *cols)
     *rows = wsz.ws_row;
     return 0;
   }
+}
+
+int getCursorPosition()
+{
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+
+  printf("\r\n");
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1)
+  {
+    if (iscntrl(c))
+    {
+      printf("%d\r\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
+
+  editorReadKey();
+  return -1;
 }
 
 /*** Input section ***/
